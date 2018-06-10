f123ec1fd534d16ab00b68dae048c7a099d33e93
Daniel Morinigo <daniel@alauda.io>
2018-06-10 15:45:08 +0800

    changing message

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("title: %s\n", title)
 	}
 
-	fmt.Fprintf(w, "Hello from:  "+title+"\n")
+	fmt.Fprintf(w, "This is a hello from:  "+title+"\n")
 }
 
 func main() {
